87ffd3671e2210883a421e10282295b77de7176c
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-01-04 11:55:23 +0700

    anything

--- pages/create-post.php
+++ pages/create-post.php
@@ -25,7 +25,7 @@ $allCat = $db->query("SELECT * FROM cat ");
 
             <div class="mb-3">
 
-                <label for="categories" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+                <label for="categories" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เลือกหมวดหมู่</label>
                 <select id="categories" name="post_cat_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                     <?php while ($cat = fetch($allCat)) : ?>
                         <option value="<?php echo $cat['cat_id']; ?>"><?php echo $cat['cat_name']; ?></option>
