a51ade2ba0ab4f05ec280f3d03b8ba76049ccefc
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-01-06 13:18:30 +0700

    fixed error poll

--- pages/poll/[].php
+++ pages/poll/[].php
@@ -1,9 +1,17 @@
+<?php
+$db = import('./Database/db');
+$getParams = import('wisit-router/getParams');
+$poll_id = (int) $getParams();
+$getPoll = $db->query("SELECT * FROM poll WHERE poll_id = $poll_id LIMIT 1");
+if ($getPoll->num_rows == 0) return import('./pages/_error');
+?>
+
 <title>แบบประเมิน | aden</title>
 <div class="row">
     <div class="col-span-3">
-    <?php import('./components/poll-user/PollSearch'); ?>
-</div>
-<div class="col-span-6 text-zinc-800">
+        <?php import('./components/poll-user/PollSearch'); ?>
+    </div>
+    <div class="col-span-6 text-zinc-800">
         <?php import('./components/poll-user/ShowPollDetail'); ?>
         <?php import('./components/poll-user/ShowChoice'); ?>
     </div>
