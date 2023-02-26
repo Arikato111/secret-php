c408dd22ef12da8b65614d60ac5fd85ffa788255
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-02-26 21:01:34 +0700

    delete token api

--- pages/api/token.php
+++ pages/api/token.php
@@ -1,5 +1,7 @@
 <?php
+$Notfound = import('./components/api/Notfound');
 $db = new Database;
+
 Wexpress::get('*', function () use ($db) {
     $header = Req::header();
     $token1 = $header['token1'] ?? null;
@@ -18,3 +20,20 @@ Wexpress::get('*', function () use ($db) {
     Res::json(['status' => 0, 'message' => 'bad request']);
     die;
 });
+
+Wexpress::delete('*', function () use ($db) {
+    $header = Req::header();
+    $token1 = $header['token1'] ?? null;
+    $token2 = $header['token2'] ?? null;
+    if ($token1 && $token2) {
+        $log = $db->getLog_ByToken($token1, $token2);
+        if ($log) {
+            $db->deleteLog_ByID($log['log_id'] ?? 0);
+            Res::json(['status' => 1, 'message' => 'delete token success']);
+        }
+    }
+    Res::json(['status' => 0, 'message' => 'bad request']);
+    die;
+});
+
+Wexpress::all('*', $Notfound);
