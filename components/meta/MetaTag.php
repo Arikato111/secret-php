d43828d3306d848ce88469d2dd755679db9dc340
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-03-24 12:11:26 +0700

    add keywords in meta tag

--- components/meta/MetaTag.php
+++ components/meta/MetaTag.php
@@ -19,7 +19,7 @@ if ($post ?? false) {
 ?>
     <meta name="title" content="สำรวจ | กระดานสนทนา | ผู้คน" />
     <meta name="description" content="aden คือแพลตฟอร์มสำหรับผู้ที่อยากพูดคุยสื่อสารกับคนอื่นๆ อย่างเรียบง่ายและไม่ซับซ้อน" />
-
+    <meta name="keywords" content="aden, โซเชียล, โซเชียล aden, เว็บออนไลน์, เว็บข่าวสาร, เว็บกระทู้, เว็บไซต์พูดคุย, เว็บกระดานข่าวสาร, เว็บอะไรสักอย่าง, นึกไม่ออกแล้ว, open source, เว็บ open source, เว็บไซต์ open source, เว็บออนไลน์ open source">
     <meta property="og:url" content="https://aden.anytion.com" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="สำรวจ | กระดานสนทนา | ผู้คน" />
